013e7c2a90322db6127ad78ef57fa6a6ac675e3a
eyal shay <eyalshay@eyals-MacBook-Air.local>
2022-08-01 15:28:59 +0100

    added getArticleById function with error handling

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,7 +13,7 @@ afterAll(() => {
 });
 
 describe("GET api/topics", () => {
-  it("status:200 , enpoint responds with JSON object containing an array of topic objects", () => {
+  test("status:200 , enpoint responds with JSON object containing an array of topic objects", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
@@ -33,13 +33,49 @@ describe("GET api/topics", () => {
   });
 });
 
-describe("ALL /*", () => {
+describe("ERROR Handling", () => {
   test("status: 404 when unable to find endpoint", () => {
     return request(app)
       .get("/missingEndpoint")
       .expect(404)
       .then(({ body }) => {
         expect(body.msg).toBe("Endpoint was not found!");
+      });
+  });
+  test("status: 400 for an invalid article_id", () => {
+    return request(app)
+      .get("/api/article/blorp")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Endpoint was not found!");
+      });
+  });
+  test("status: 404 for a valid, but non existing article_id", () => {
+    return request(app)
+      .get("/api/articles/9000")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Article not found!");
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("responds with a single matching article", () => {
+    const ID = 2;
+    return request(app)
+      .get(`/api/articles/${ID}`)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toEqual({
+          article_id: ID,
+          title: "Sony Vaio; or, The Laptop",
+          topic: "mitch",
+          author: "icellusedkars",
+          body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+          created_at: expect.any(String),
+          votes: 0,
+        });
       });
   });
 });
--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const app = express();
 const seed = require("./db/seeds/seed");
-const { getTopics } = require("./controllers/topics-controllers");
+const {
+  getTopics,
+  getArticlesById,
+} = require("./controllers/topics-controllers");
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticlesById);
+
 app.all("/*", (req, res) => {
+  console.log("<<< error in app.all");
   res.status(404).send({ msg: "Endpoint was not found!" });
 });
 
 app.use((err, req, res, next) => {
-  console.log(err.msg, "<<< app.js");
-  res.status(err.status).send({ msg: err.msg });
+  console.log(err, "<<< error in app.use");
+  if (err.code === "to be added") {
+    res.status(400).send({ msg: "Invalid request!" });
+  } else {
+    res.status(err.status).send({ msg: err.msg });
+  }
 });
 
 module.exports = app;
